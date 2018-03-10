b27ce97cd50df3e678d5f8cc31e3fc6f4c938bfd
Neil Johnson <neil.johnson@erudicon.com>
2018-03-10 11:27:30 -0600

    Finished Chapter 8

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,7 @@ def before_request():
 		current_user.last_seen = datetime.utcnow()
 		db.session.commit()
 		
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -30,6 +31,7 @@ def index():
     ]
     return render_template('index.html', title='Home',  posts=posts)
     
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -47,11 +49,13 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
     
+
 @app.route('/register', methods=['GET','POST'])
 def register():
 	if current_user.is_authenticated:
@@ -66,6 +70,7 @@ def register():
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
 	
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -76,6 +81,7 @@ def user(username):
 	]
 	return render_template('user.html', user=user, posts=posts)
 	
+
 @app.route('/edit_profile', methods=['GET','POST'])
 @login_required
 def edit_profile():
@@ -91,3 +97,36 @@ def edit_profile():
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', title='Edit Profile',
 							form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot follow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.follow(user)
+	db.session.commit()
+	flash('You are following {}!'.format(username))
+	return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found'.format(username))
+		return redirect(url_for('index'))
+	if user == current_user:
+		flash('You cannot follow yourself!')
+		return redirect(url_for('user', username=username))
+	current_user.unfollow(user)
+	db.session.commit()
+	flash('You are not following {}.'.format(username))
+	return redirect(url_for('user', username=username))
+
+
+
